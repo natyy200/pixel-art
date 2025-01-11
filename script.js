86cb9133e6076c9e66972f62558dba298992e54c
natyy200 <nataly.schmidt@escola.pr.gov.br>
2025-01-11 13:28:01 -0300

    começando js e criando elementos

---
+++ script.js
@@ -0,0 +1,34 @@
+const canvas = document.querySelector(".canvas");
+const inputSize = document.querySelector(".input-size");
+const inputColor = document.querySelector(".input-color");
+const usedColors = document.querySelector(".used-colors");
+const buttonSave = document.querySelector(".button-save");
+const colResize = document.querySelector(".resize");
+const main = document.querySelector("main");
+
+const createElement = (tag, className = "") => {
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+}
+
+const createPixel = () => {
+    const pixel = createElement("div", "pixel");
+    return pixel;
+}
+
+const loadCanvas = () => {
+    const length = inputSize.value;
+
+    for ( let i = 0; i < length; i += 1 ) {
+        const row = createElement("div", "row");
+
+        for ( let j = 0; j < length; j += 1 ) {
+            row.append(createPixel());
+        }
+
+        canvas.append(row);
+    }
+}
+
+loadCanvas();
